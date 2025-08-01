df105f00da8a78d160f96ae74302886297bb0ca9
Tiago Cardoso <tiago.cardoso@encora.com>
2025-08-01 09:46:40 -0300

    update configs

--- nvim/.config/nvim/lua/tiggas/plugins/copilot.lua
+++ nvim/.config/nvim/lua/tiggas/plugins/copilot.lua
@@ -5,6 +5,7 @@ return {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
+    event = { "BufReadPre", "BufNewFile" },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
