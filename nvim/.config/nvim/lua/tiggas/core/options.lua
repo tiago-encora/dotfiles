b7b595f4ffe1ae6cca79abf018c68bca8d51efea
Tiago Cardoso <tiago.cardoso@encora.com>
2025-11-05 16:40:19 -0300

    fix nvim auto-session

--- nvim/.config/nvim/lua/tiggas/core/options.lua
+++ nvim/.config/nvim/lua/tiggas/core/options.lua
@@ -1,3 +1,4 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
 vim.api.nvim_set_keymap("n", "<C-c>", "y", { noremap = true })
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
